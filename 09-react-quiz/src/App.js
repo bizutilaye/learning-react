f00badffbf054eb0c333c42cb9f1f5d0c6c1082a
react <bizualemtilaye@gmail.com>
2024-12-16 02:27:56 -0800

    useReducer

---
+++ 09-react-quiz/src/App.js
@@ -0,0 +1,10 @@
+import Header from "./Header";
+
+import DateCounter from "./DateCounter";
+export default function App() {
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
+}
