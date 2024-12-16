656b5bd63bcc8b71b97e1beccb015f76102a1a26
react <bizualemtilaye@gmail.com>
2024-12-16 03:03:18 -0800

    React quic app

--- 09-react-quiz/src/App.js
+++ 09-react-quiz/src/App.js
@@ -1,10 +1,14 @@
 import Header from "./Header";
+import Main from "./Main";
 
-import DateCounter from "./DateCounter";
 export default function App() {
   return (
     <div className="app">
       <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
     </div>
   );
 }
