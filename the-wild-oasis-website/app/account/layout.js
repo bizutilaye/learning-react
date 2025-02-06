23737034e7c4e5ada392a9e2932e9bd9f4611f8f
react <bizualemtilaye@gmail.com>
2025-02-05 23:30:23 -0800

    Nested Layout

--- the-wild-oasis-website/app/account/layout.js
+++ the-wild-oasis-website/app/account/layout.js
@@ -1,7 +1,9 @@
+import SideNavigation from "@/app/_component/SideNavigation";
+
 export default function layout({ children }) {
   return (
     <div className="grid grid-cols-[16rem_1fr] h-full gap-12">
-      <div>navigation</div>
+      <SideNavigation />
       <div className="py-1">{children}</div>
     </div>
   );
